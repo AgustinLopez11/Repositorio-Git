83e457f7b578384156d47fef8a1749e3aad74f01
Santos Lopez <agus77chang@gmail.com>
2025-05-05 14:35:59 -0600

    Actualizacion de archivos

---
+++ app.js
@@ -0,0 +1,8 @@
+let boton= document.getElementById('boton')
+let body= document.querySelector('body')
+
+
+function negro(){
+    body.style.background="black"
+}
+boton.addEventListener("click", negro)
